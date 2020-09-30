e40a6a549ef1161535e2720a99c074fbaa67a2eb
tim <tim@glo.id.au>
2020-09-30 17:13:21 +1000

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,71 @@
-import { useState } from "haunted";
-import { html } from 'lit-element';
+// import { useState } from "haunted";
+import { html, LitElement } from 'lit-element';
 
-import { componentWithHooks } from './with-hooks';
+// import { componentWithHooks } from './with-hooks';
 import "./style.css";
 import "./FullName";
 
-function App() {
-  const [name, setName] = useState("");
+export class App extends LitElement {
+  name: string;
 
-  return html`
-    <h2>User Page</h2>
-    <h3>${name}</h3>
+  static get properties() {
+    return {
+      name: { type: String },
+    };
+  }
 
-    <fieldset>
-      <legend>Change name:</legend>
-      <full-name @change=${(ev) => setName(ev.detail)}></full-name>
-    </fieldset>
+  private setName(name) {
+    console.log('@@@@@@@@@@', name);
+    this.name = name;
+  }
 
-    <style>
-      fieldset {
-        border: none;
-      }
+  render() {
+    return html`
+      <h2>User Page</h2>
+      <h3>${this.name}</h3>
 
-      legend {
-        padding: 0;
-        margin-bottom: 0.5rem;
-      }
-    </style>
-  `;
+      <fieldset>
+        <legend>Change name:</legend>
+        <full-name @change=${(ev) => this.setName(ev.detail)}></full-name>
+      </fieldset>
+
+      <style>
+        fieldset {
+          border: none;
+        }
+
+        legend {
+          padding: 0;
+          margin-bottom: 0.5rem;
+        }
+      </style>
+    `;
+  }
 }
 
-customElements.define("my-app", componentWithHooks({ renderer: App }));
+// function App() {
+//   const [name, setName] = useState("");
+
+//   return html`
+//     <h2>User Page</h2>
+//     <h3>${name}</h3>
+
+//     <fieldset>
+//       <legend>Change name:</legend>
+//       <full-name @change=${(ev) => setName(ev.detail)}></full-name>
+//     </fieldset>
+
+//     <style>
+//       fieldset {
+//         border: none;
+//       }
+
+//       legend {
+//         padding: 0;
+//         margin-bottom: 0.5rem;
+//       }
+//     </style>
+//   `;
+// }
+
+customElements.define("my-app", App);
