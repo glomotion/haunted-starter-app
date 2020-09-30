32839c6e6a8db5d7792cb9dc349a0dad624ae313
tim <tim@glo.id.au>
2020-09-30 17:23:58 +1000

    revert to use old haunted app

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,33 @@
-// import { useState } from "haunted";
+import { useState } from "haunted";
 import { html, LitElement } from 'lit-element';
 
-// import { componentWithHooks } from './with-hooks';
 import "./style.css";
 import "./FullName";
 
-export class App extends LitElement {
-  name: string;
 
-  static get properties() {
-    return {
-      name: { type: String },
-    };
-  }
+function App() {
+  const [name, setName] = useState("");
 
-  private setName(name) {
-    console.log('@@@@@@@@@@', name);
-    this.name = name;
-  }
+  return html`
+    <h2>User Page</h2>
+    <h3>${name}</h3>
 
-  render() {
-    return html`
-      <h2>User Page</h2>
-      <h3>${this.name}</h3>
+    <fieldset>
+      <legend>Change name:</legend>
+      <full-name @change=${(ev) => setName(ev.detail)}></full-name>
+    </fieldset>
 
-      <fieldset>
-        <legend>Change name:</legend>
-        <full-name @change=${(ev) => this.setName(ev.detail)}></full-name>
-      </fieldset>
+    <style>
+      fieldset {
+        border: none;
+      }
 
-      <style>
-        fieldset {
-          border: none;
-        }
-
-        legend {
-          padding: 0;
-          margin-bottom: 0.5rem;
-        }
-      </style>
-    `;
-  }
+      legend {
+        padding: 0;
+        margin-bottom: 0.5rem;
+      }
+    </style>
+  `;
 }
-
-// function App() {
-//   const [name, setName] = useState("");
-
-//   return html`
-//     <h2>User Page</h2>
-//     <h3>${name}</h3>
-
-//     <fieldset>
-//       <legend>Change name:</legend>
-//       <full-name @change=${(ev) => setName(ev.detail)}></full-name>
-//     </fieldset>
-
-//     <style>
-//       fieldset {
-//         border: none;
-//       }
-
-//       legend {
-//         padding: 0;
-//         margin-bottom: 0.5rem;
-//       }
-//     </style>
-//   `;
-// }
 
 customElements.define("my-app", App);
